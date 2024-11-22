fbd6910a8e6accd6965083dac728190b4430db4b
Kazik6maziK <dimaredlin715@gmail.com>
2024-11-23 00:16:30 +0300

    откат

--- js/game.js
+++ js/game.js
@@ -1,43 +1,19 @@
-// Индексы для каждого слайдера
-let currentSlideIllustrations = 0;
-let currentSlideMoments = 0;
+let currentSlide = 0;
 
-// Функция для показа слайдов с учетом направления и типа слайдера
-function showSlide(index, type) {
-  let slides;
-  let totalSlides;
-
-  // В зависимости от типа слайдера (illustrations или moments)
-  if (type === 'illustrations') {
-    slides = document.querySelectorAll('#illustrations-slider .slide');
-    totalSlides = slides.length;
-    if (index >= totalSlides) currentSlideIllustrations = 0;
-    if (index < 0) currentSlideIllustrations = totalSlides - 1;
-  } else if (type === 'moments') {
-    slides = document.querySelectorAll('#moments-slider .funny-slide');
-    totalSlides = slides.length;
-    if (index >= totalSlides) currentSlideMoments = 0;
-    if (index < 0) currentSlideMoments = totalSlides - 1;
-  }
-
+function showSlide(index) {
+  const slides = document.querySelectorAll('.slide');
+  if (index >= slides.length) currentSlide = 0;
+  if (index < 0) currentSlide = slides.length - 1;
+  
   const slideWidth = slides[0].clientWidth;
-  const slider = document.querySelector(`#${type}-slider`);
-  slider.style.transform = `translateX(-${(type === 'illustrations' ? currentSlideIllustrations : currentSlideMoments) * slideWidth}px)`;
+  const slider = document.querySelector('.slider');
+  slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
 
-// Функция для переключения слайдов
-function moveSlide(direction, type) {
-  if (type === 'illustrations') {
-    currentSlideIllustrations += direction;
-    showSlide(currentSlideIllustrations, 'illustrations');
-  } else if (type === 'moments') {
-    currentSlideMoments += direction;
-    showSlide(currentSlideMoments, 'moments');
-  }
+function moveSlide(direction) {
+  currentSlide += direction;
+  showSlide(currentSlide);
 }
 
 // Инициализация слайдера
-document.addEventListener('DOMContentLoaded', () => {
-  showSlide(currentSlideIllustrations, 'illustrations');
-  showSlide(currentSlideMoments, 'moments');
-});
+showSlide(currentSlide);
