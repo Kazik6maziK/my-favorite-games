0321317ebb9fa34d53421912956264ac92496e77
Kazik6maziK <dimaredlin715@gmail.com>
2024-11-23 00:32:19 +0300

    Тестирую слайдеры

--- js/game.js
+++ js/game.js
@@ -1,28 +1,32 @@
-// Инициализация слайдера
-function initSlider(sliderElement) {
-    let currentSlide = 0;
-
-    const slides = sliderElement.querySelectorAll('.slide');
+// Функция для отображения слайда в конкретном слайдере
+function showSlide(sliderId, index) {
+    const slides = document.querySelectorAll(`#${sliderId} .slide`);
+    const slider = document.querySelector(`#${sliderId}`);
+    
+    // Проверяем границы индекса
+    if (index >= slides.length) {
+      currentSlide[sliderId] = 0;
+    } else if (index < 0) {
+      currentSlide[sliderId] = slides.length - 1;
+    } else {
+      currentSlide[sliderId] = index;
+    }
+  
     const slideWidth = slides[0].clientWidth;
-    const slider = sliderElement.querySelector('.slider');
-
-    function showSlide(index) {
-        if (index >= slides.length) currentSlide = 0;
-        if (index < 0) currentSlide = slides.length - 1;
-        
-        slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-    }
-
-    function moveSlide(direction) {
-        currentSlide += direction;
-        showSlide(currentSlide);
-    }
-
-    sliderElement.querySelector('.prev').addEventListener('click', () => moveSlide(-1));
-    sliderElement.querySelector('.next').addEventListener('click', () => moveSlide(1));
-
-    showSlide(currentSlide); // Показать первый слайд
-}
-
-// Применяем функцию ко всем слайдерам на странице
-document.querySelectorAll('.slider-container').forEach(initSlider);
+    slider.style.transform = `translateX(-${currentSlide[sliderId] * slideWidth}px)`;
+  }
+  
+  // Функция для изменения слайда в конкретном слайдере
+  function moveSlide(direction, sliderId) {
+    currentSlide[sliderId] += direction;
+    showSlide(sliderId, currentSlide[sliderId]);
+  }
+  
+  // Инициализация слайдеров
+  let currentSlide = {};
+  document.querySelectorAll('.slider').forEach(slider => {
+    const sliderId = slider.id;
+    currentSlide[sliderId] = 0;
+    showSlide(sliderId, currentSlide[sliderId]);
+  });
+  
