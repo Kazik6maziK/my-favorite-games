472765cf77bb3f1c0e63bac60f80b3868db3c922
Kazik6maziK <dimaredlin715@gmail.com>
2024-11-23 00:26:11 +0300

    Тестирую

--- js/game.js
+++ js/game.js
@@ -1,19 +1,28 @@
-let currentSlide = 0;
-
-function showSlide(index) {
-  const slides = document.querySelectorAll('.slide');
-  if (index >= slides.length) currentSlide = 0;
-  if (index < 0) currentSlide = slides.length - 1;
-  
-  const slideWidth = slides[0].clientWidth;
-  const slider = document.querySelector('.slider');
-  slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-}
-
-function moveSlide(direction) {
-  currentSlide += direction;
-  showSlide(currentSlide);
-}
-
 // Инициализация слайдера
-showSlide(currentSlide);
+function initSlider(sliderElement) {
+    let currentSlide = 0;
+
+    const slides = sliderElement.querySelectorAll('.slide');
+    const slideWidth = slides[0].clientWidth;
+    const slider = sliderElement.querySelector('.slider');
+
+    function showSlide(index) {
+        if (index >= slides.length) currentSlide = 0;
+        if (index < 0) currentSlide = slides.length - 1;
+        
+        slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+    }
+
+    function moveSlide(direction) {
+        currentSlide += direction;
+        showSlide(currentSlide);
+    }
+
+    sliderElement.querySelector('.prev').addEventListener('click', () => moveSlide(-1));
+    sliderElement.querySelector('.next').addEventListener('click', () => moveSlide(1));
+
+    showSlide(currentSlide); // Показать первый слайд
+}
+
+// Применяем функцию ко всем слайдерам на странице
+document.querySelectorAll('.slider-container').forEach(initSlider);
