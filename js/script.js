82f18f9707b523c6fc4c5c45b093c2d527f677ef
Kazik6maziK <dimaredlin715@gmail.com>
2024-11-23 00:12:51 +0300

    Тестирую

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function showSlide(index) {
   const slides = document.querySelectorAll('.slide');
   if (index >= slides.length) currentSlide = 0;
   if (index < 0) currentSlide = slides.length - 1;
-
+  
   const slideWidth = slides[0].clientWidth;
   const slider = document.querySelector('.slider');
   slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
